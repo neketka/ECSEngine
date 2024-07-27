f659766abf392e19899196a2167832720373f5f7
neketka <neketka@gmail.com>
2024-07-26 20:27:08 -0400

    Add query implementation skeletons

--- ECSTest/EcsStorage.h
+++ ECSTest/EcsStorage.h
@@ -15,7 +15,7 @@ private:
 	static inline constexpr bool ContainsInternal = ContainsInternal<TTarget, TComp> || ContainsInternal<TTarget, TComps...>;
 
 	template<typename TArchOther, typename TComp>
-	using UnionParts = std::conditional<TArchOther::Contains<TComp>, TArchOther, TArchOther::AppendNoUnion<TComp>>;
+	using UnionParts = std::conditional<TArchOther::template Contains<TComp>, TArchOther, TArchOther::template AppendNoUnion<TComp>>;
 
 	template<typename TArchOther, typename TComp, typename... TComps>
 	using UnionParts = UnionParts<UnionParts<TArchOther, TComp>, TComps...>;
@@ -35,13 +35,52 @@ public:
 	static inline constexpr bool Contains = ContainsInternal<TComp, TComponents...>;
 
 	template<typename TArchSuperset>
-	static inline constexpr bool IsSubsetOf = TArchSuperset::Contains<TComponents> && ...;
+	static inline constexpr bool IsSubsetOf = TArchSuperset::template Contains<TComponents> && ...;
 
 	template<typename TArchSuperset>
-	static inline constexpr bool AnyIn = TArchSuperset::Contains<TComponents> || ...;
+	static inline constexpr bool AnyIn = TArchSuperset::template Contains<TComponents> || ...;
 };
 
 using EmptyArchetype = Archetype<>;
+
+template<
+	typename TLevelTraverseRelation, typename TExcludedArch, typename TContainsOrExprs,
+	typename TRelationArchPath, typename TUsedComponentsArch, typename... TReadsWrites
+>
+class QueryImpl;
+
+// Simple sequential
+template<
+	typename TExcludedArch, typename TContainsOrExprs,
+	typename TUsedComponentsArch, typename... TReadsWrites
+>
+class QueryImpl<std::monostate, TExcludedArch, TContainsOrExprs, EmptyArchetype, TUsedComponentsArch, TReadsWrites...>
+{
+public:
+
+};
+
+// Relational
+template<
+	typename TExcludedArch, typename TContainsOrExprs, typename TRelationArchPath,
+	typename TUsedComponentsArch, typename... TReadsWrites
+>
+class QueryImpl<std::monostate, TExcludedArch, TContainsOrExprs, TRelationArchPath, TUsedComponentsArch, TReadsWrites...>
+{
+public:
+
+};
+
+// BFS Relation Tree
+template<
+	typename TLevelTraverseRelation, typename TExcludedArch, typename TContainsOrExprs,
+	typename TUsedComponentsArch, typename... TReadsWrites
+>
+class QueryImpl<TLevelTraverseRelation, TExcludedArch, TContainsOrExprs, EmptyArchetype, TUsedComponentsArch, TReadsWrites...>
+{
+public:
+
+};
 
 template<
 	typename TLevelTraverseRelation, typename TExcludedArch, typename TContainsOrExprs, 
@@ -50,31 +89,31 @@ template<
 class QueryBase
 {
 public:
-	template<typename... TComponents> requires !TUsedComponentsArch::AnyIn<Archetype<TComponents...>>
+	template<typename... TComponents> requires !TUsedComponentsArch::template AnyIn<Archetype<TComponents...>>
 	using Read = 
 		QueryBase<
 			TLevelTraverseRelation, TExcludedArch, TContainsOrExprs, TRelationArchPath,
-			TUsedComponentsArch::Append<TComponents...>, TReadsWrites..., const TComponents...
+			TUsedComponentsArch::template Append<TComponents...>, TReadsWrites..., const TComponents...
 		>;
 
-	template<typename... TComponents> requires !TUsedComponentsArch::AnyIn<Archetype<TComponents...>>
+	template<typename... TComponents> requires !TUsedComponentsArch::template AnyIn<Archetype<TComponents...>>
 	using Write =
 		QueryBase<
 			TLevelTraverseRelation, TExcludedArch, TContainsOrExprs, TRelationArchPath,
-			TUsedComponentsArch::Append<TComponents...>, TReadsWrites..., TComponents...
+			TUsedComponentsArch::template Append<TComponents...>, TReadsWrites..., TComponents...
 		>;
 
 	template<typename... TComponents>
 	using ContainingAll =
 		QueryBase<
-			TLevelTraverseRelation, TExcludedArch, TContainsOrExprs::Append<Archetype<TComponents...>>,
+			TLevelTraverseRelation, TExcludedArch, TContainsOrExprs::template Append<Archetype<TComponents...>>,
 			TRelationArchPath, TUsedComponentsArch, TReadsWrites...
 		>;
 
 	template<typename... TComponents>
 	using Exclude =
 		QueryBase<
-			TLevelTraverseRelation, TExcludedArch::Append<TComponents...>, TContainsOrExprs,
+			TLevelTraverseRelation, TExcludedArch::template Append<TComponents...>, TContainsOrExprs,
 			TRelationArchPath, TUsedComponentsArch, TReadsWrites...
 		>;
 
